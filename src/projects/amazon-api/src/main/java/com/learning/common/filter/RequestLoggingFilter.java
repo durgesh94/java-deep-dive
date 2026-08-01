0811a306bd73985629d58868936a3ae21542f0ff
Durgesh Tambe <dntambe@gmail.com>
2026-08-01 13:13:48 +0530

    filters

---
+++ src/projects/amazon-api/src/main/java/com/learning/common/filter/RequestLoggingFilter.java
@@ -0,0 +1,44 @@
+package com.learning.common.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class RequestLoggingFilter extends OncePerRequestFilter {
+
+    private static final Logger logger =
+            LoggerFactory.getLogger(RequestLoggingFilter.class);
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+
+
+        logger.info("Incoming Request : {} {}",
+                request.getMethod(),
+                request.getRequestURI());
+
+        long start = System.currentTimeMillis();
+
+        filterChain.doFilter(request, response);
+
+        long end = System.currentTimeMillis();
+
+        logger.info("API {} executed in {} ms",
+                request.getRequestURI(),
+                end - start);
+
+        logger.info("Outgoing Response : {}",
+                response.getStatus());
+    }
+}
